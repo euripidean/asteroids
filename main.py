62ada537b8a159fd547a807337cae16829202b32
Jane Harrison <33559193+euripidean@users.noreply.github.com>
2024-10-20 18:21:45 -0700

    Added container groups

--- main.py
+++ main.py
@@ -10,6 +10,14 @@ def main():
     Clock = pygame.time.Clock()
     dt = 0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    
+
+    # Groups
+    updateable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+
+    # Add player to groups
+    Player.containers = (updateable, drawable)
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
 
     while True:
@@ -17,8 +25,13 @@ def main():
             if event.type == pygame.QUIT:
                 return
         screen.fill((0,0,0))
-        player.draw(screen)
-        player.update(dt)
+        for sprite in updateable:
+            sprite.draw(screen)
+        
+        for sprite in drawable:
+            sprite.update(dt)
+
+
         pygame.display.flip()
         delta = Clock.tick(60)
         dt = delta / 1000
